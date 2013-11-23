1b7e8b6b496fdac603aeb3ad8a6d322129c777f3
Andrew Pairman <hello@andrewpairman.com>
2013-11-23 14:49:08 +0000

    Styling updates

--- app/views/post/index.blade.php
+++ app/views/post/index.blade.php
@@ -8,11 +8,10 @@
 		    <a target="_blank" href="{{ $row->url }}"><h2>{{ $row->title }}</h2></a>
 		    <p><span class="domain">(<a target="_blank" href="{{ $row->url }}">{{ $row->domain }}</a>)</span> 
 		       <span class="sub-text">Posted by {{ $row->User->first_name }} {{ substr ( $row->User->last_name , 0, 1 ) }} {{ $row->timeSince() }}<?php /* | 3 comments*/?></span>
-		       <span class="tags">
+		       <br>
 		       @foreach($row->tags as $tag)
-		       #{{$tag->name}}
+		       <a class="tags">{{$tag->name}}</a>
 		       @endforeach
-		       </span>
 		    </p>
 		</div>
 	</div>
