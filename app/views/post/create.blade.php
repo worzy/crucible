2928669cfa0018f0331718d2805905bacfa59549
Andrew Pairman <hello@andrewpairman.com>
2013-11-23 14:49:08 +0000

    Styling updates

--- app/views/post/create.blade.php
+++ app/views/post/create.blade.php
@@ -1,12 +1,12 @@
 @section('content')
 {{ Form::open(array('url' => 'post')) }}
 @foreach ($errors->all() as $error)
-    <p>{{ $error }}</p>
+    <p><span class="error">&times;</span> {{ $error }}</p>
 @endforeach
 <label for="title">Title</label> <input type="text" name="title" placeholder="What's the subject of the link?">
 <label for="url">URL</label> <input type="text" name="url" placeholder="Paste the link URL here">
-<label for="tags">Tags</label> <input type="text" name="tags" placeholder="Use a comma to seperate your tag">
+<label for="tags">Tags</label> <input type="text" name="tags" placeholder="Use a comma to seperate your tags">
 <?php //<label for="content">Description</label> <textarea name="content"></textarea>?>
-<input type="submit" value="Submit">
+<input class="btn" type="submit" value="Share">
 {{ Form::close() }}
 @stop
--- app/views/user/register.blade.php
+++ app/views/user/register.blade.php
@@ -3,7 +3,7 @@
 <div class="col6">
 	<fieldset>
 		<label for="name">Name</label> <input type="text" name="first_name" placeholder="John Smith?">
-		<label for="email">Email</label> <input type="email" name="email" placeholder="We can pull your Gravatar using this.">
+		<label for="email">Email</label> <input type="email" name="email" placeholder="We will pull your Gravatar using this.">
 		<label for="password">Password</label> <input type="password" name="password" placeholder="Is it a secret? Is it safe?">
 		<input class="btn" type="submit" value="Sign up">
 	</fieldset>
