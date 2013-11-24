0185ef7f4fd22081932c6fc885f5aa1401b39cd9
Andrew Pairman <hello@andrewpairman.com>
2013-11-24 12:08:47 +0000

    More styling updates

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -4,7 +4,7 @@
 <fieldset>
     <label for="email">Email address</label> <input type="email" name="email" placeholder="Enter your email">
 	<label for="password">Password</label> <input type="password" name="password" placeholder="Enter your password">
-	<input class="btn" type="submit" value="Log in">
+	<input class="btn" type="submit" value="Log in"><a class="btn_back" href="/">Back</a>
 </fieldset>
 {{ Form::close() }}
 </div>
