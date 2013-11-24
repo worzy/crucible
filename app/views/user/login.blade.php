32475b7e68ce5f8cf24bd4ba008efc183ed7bc0b
Gareth Drew <garethdrew@gmail.com>
2013-11-24 17:13:36 +0000

    Fix missing class on back buttons

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -7,7 +7,7 @@
 <fieldset>
     <label for="email">Email address</label> <input type="email" name="email" placeholder="Enter your email">
 	<label for="password">Password</label> <input type="password" name="password" placeholder="Enter your password">
-	<input class="btn" type="submit" value="Log in"><a class="btn_back" href="/">Back</a>
+	<input class="btn" type="submit" value="Log in"><a class="btn btn_back" href="/">Back</a>
 </fieldset>
 {{ Form::close() }}
 </div>
