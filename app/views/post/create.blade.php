ddd168f075886174efe148578b61567370e9307e
Andrew Pairman <hello@andrewpairman.com>
2014-01-12 22:16:04 +0000

    Responsive tweaks with a shiny logo

--- app/views/post/create.blade.php
+++ app/views/post/create.blade.php
@@ -7,7 +7,7 @@
 	<h2>Share something awesome!</h2>
 	<label for="title">Title</label> <input type="text" name="title" placeholder="What's the subject of the link?" value="{{$bookmark['title']}}{{ Input::old('title') }}" autofocus>
 	<label for="url">URL</label> <input type="text" name="url" placeholder="Paste the link URL here" value="{{$bookmark['url']}}{{ Input::old('url') }}">
-	<label for="tags">Tags</label> <input type="text" name="tags" placeholder="Use a comma to seperate your tags" value="{{ Input::old('tags') }}">
+	<label for="tags">Tag it</label> <input type="text" name="tags" placeholder="Use a comma to seperate your tags" value="{{ Input::old('tags') }}">
 	<?php //<label for="content">Description</label> <textarea name="content"></textarea>?>
 	<input class="btn" type="submit" value="Share"> <a class="btn btn_back" href="/">Back</a>
 
