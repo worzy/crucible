d3dfef18bf411a7f664bd467675987ade2ea88ef
Andrew Pairman <hello@andrewpairman.com>
2014-05-21 17:49:19 +0100

    Thermometer animation tests

--- app/views/post/index.blade.php
+++ app/views/post/index.blade.php
@@ -1,4 +1,4 @@
-@section('content') 
+@section('content')
 @if(!empty($tag_title))
 <div class="clear"></div>
 <div class="filter">
@@ -8,7 +8,7 @@
 
 @if($posts->count() > 0)
     @foreach ($posts as $row)
-  
+
 
 	<div class="post col8 alpha omega">
 		<div class="left">
@@ -16,7 +16,7 @@
 		</div>
 		<div class="right">
 		    <a target="_blank" href="{{ $row->url }}"><h2>{{ $row->title }}</h2></a>
-		    <span class="domain"><a target="_blank" href="{{ $row->url }}">({{ $row->domain }})</a></span> 
+		    <span class="domain"><a target="_blank" href="{{ $row->url }}">({{ $row->domain }})</a></span>
 		       <span class="sub-text">Posted by {{ $row->User->first_name }} {{ substr ( $row->User->last_name , 0, 1 ) }} {{ $row->timeSince() }} - </span> <span class="comments"> {{HTML::linkRoute('post.show', count($row->comments).' comments', array($row->id))}}</span>
 		       <br>
 		       <p class="tags">@foreach($row->tags as $tag)
@@ -24,6 +24,9 @@
 		       @endforeach</p>
 		</div>
 	</div>
+
+<div class="thermometer">
+</div>
 
 <div class="clear"></div>
 @endforeach
@@ -45,4 +48,4 @@
 <?php echo $posts->links(); ?>
 
 
-@stop 
+@stop
